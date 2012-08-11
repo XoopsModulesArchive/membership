4e16ee745097f17608c064fd77183a00c2256519
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- htdocs/modules/membership/language/english/admin.php
+++ htdocs/modules/membership/language/english/admin.php
@@ -1,61 +1,61 @@
 <?php
 	// Packages Listing
-	define('_MEM_AM_PACKAGESLIST_H1', 'Packages');
-	define('_MEM_AM_PACKAGESLIST_P', 'Below is a sortable list of packages. You can edit and change package details on bulk or do one of the actions.');
-	define('_MEM_AM_TH_WEIGHT', 'Weight');
-	define('_MEM_AM_TH_TITLE', 'Title');
-	define('_MEM_AM_TH_CURRENCY', 'ISO Currency');
-	define('_MEM_AM_TH_PRICE', 'Price');
-	define('_MEM_AM_TH_PERIOD', 'Seconds');
-	define('_MEM_AM_TH_PERIOD_TEXT', 'Period');
-	define('_MEM_AM_TH_GROUPS', 'Groups');
-	define('_MEM_AM_TH_LAST', 'Last Purchased');
-	define('_MEM_AM_TH_PURCHASES', 'Purchases');
-	define('_MEM_AM_TH_ACTIONS', 'Actions');
+	define('_MEM_AM_PACKAGESLIST_H1','Packages');
+	define('_MEM_AM_PACKAGESLIST_P','Below is a sortable list of packages. You can edit and change package details on bulk or do one of the actions.');
+	define('_MEM_AM_TH_WEIGHT','Weight');
+	define('_MEM_AM_TH_TITLE','Title');
+	define('_MEM_AM_TH_CURRENCY','ISO Currency');
+	define('_MEM_AM_TH_PRICE','Price');
+	define('_MEM_AM_TH_PERIOD','Seconds');
+	define('_MEM_AM_TH_PERIOD_TEXT','Period');
+	define('_MEM_AM_TH_GROUPS','Groups');
+	define('_MEM_AM_TH_LAST','Last Purchased');
+	define('_MEM_AM_TH_PURCHASES','Purchases');
+	define('_MEM_AM_TH_ACTIONS','Actions');
 
 	//Packages edit/new form
-	define('_MEM_AM_PACKAGE_H1', 'Package');
-	define('_MEM_AM_PACKAGE_P', 'Below is a form for a package, you can edit/create this package with this form.');
-	define('_MEM_AM_PACKAGE_NEW', 'Create New ');
-	define('_MEM_AM_PACKAGE_EDIT', 'Edit ');
-	define('_MEM_AM_FRM_PACKAGE_EDIT', 'Edit Package');
-	define('_MEM_AM_FRM_PACKAGE_NEW', 'New Package');
-	define('_MEM_AM_FRM_PACKAGE_WEIGHT', 'Weight');
-	define('_MEM_AM_FRM_PACKAGE_TITLE', 'Package Title');
-	define('_MEM_AM_FRM_PACKAGE_DESCRIPTION', 'Package Description');
-	define('_MEM_AM_FRM_PACKAGE_CURRENCY', 'Currency');
-	define('_MEM_AM_FRM_PACKAGE_PRICE', 'Price');
-	define('_MEM_AM_FRM_PACKAGE_PERIOD', 'Period (seconds)');
-	define('_MEM_AM_FRM_PACKAGE_PERIOD_TEXT', 'Period Description');
-	define('_MEM_AM_FRM_PACKAGE_GROUPS', 'Package Groups');
-	define('_MEM_AM_FRM_PACKAGE_CREATED', 'Created');
-	define('_MEM_AM_FRM_PACKAGE_UPDATED', 'Updated');
-	define('_MEM_AM_FRM_PACKAGE_LAST', 'Last Purchased');
-	define('_MEM_AM_FRM_PACKAGE_PURCHASES', 'Number of Purchases');
+	define('_MEM_AM_PACKAGE_H1','Package');
+	define('_MEM_AM_PACKAGE_P','Below is a form for a package, you can edit/create this package with this form.');
+	define('_MEM_AM_PACKAGE_NEW','Create New ');
+	define('_MEM_AM_PACKAGE_EDIT','Edit ');
+	define('_MEM_AM_FRM_PACKAGE_EDIT','Edit Package');
+	define('_MEM_AM_FRM_PACKAGE_NEW','New Package');
+	define('_MEM_AM_FRM_PACKAGE_WEIGHT','Weight');
+	define('_MEM_AM_FRM_PACKAGE_TITLE','Package Title');
+	define('_MEM_AM_FRM_PACKAGE_DESCRIPTION','Package Description');
+	define('_MEM_AM_FRM_PACKAGE_CURRENCY','Currency');
+	define('_MEM_AM_FRM_PACKAGE_PRICE','Price');
+	define('_MEM_AM_FRM_PACKAGE_PERIOD','Period (seconds)');
+	define('_MEM_AM_FRM_PACKAGE_PERIOD_TEXT','Period Description');
+	define('_MEM_AM_FRM_PACKAGE_GROUPS','Package Groups');
+	define('_MEM_AM_FRM_PACKAGE_CREATED','Created');
+	define('_MEM_AM_FRM_PACKAGE_UPDATED','Updated');
+	define('_MEM_AM_FRM_PACKAGE_LAST','Last Purchased');
+	define('_MEM_AM_FRM_PACKAGE_PURCHASES','Number of Purchases');
 	
 	// Messages
-	define('_MEM_AM_MSG_CONFIRM_DELETE', 'Are you sure you wish to delete this package?');
-	define('_MEM_AM_MSG_PACKAGE_DELETED', 'Package Succesfully Deleted!');
-	define('_MEM_AM_MSG_PACKAGES_SAVED', 'Package(s) successfully saved!');
+	define('_MEM_AM_MSG_CONFIRM_DELETE','Are you sure you wish to delete this package?');
+	define('_MEM_AM_MSG_PACKAGE_DELETED','Package Succesfully Deleted!');
+	define('_MEM_AM_MSG_PACKAGES_SAVED','Package(s) successfully saved!');
 	
 	//Permissions
-	define('_MEM_AM_PERM_ACTION', 'Action');
-	define('_MEM_AM_PERM_ACCESS_HEADER', 'Access Header');
-	define('_MEM_AM_PERM_ACCESS_DESC', 'Access');
+	define('_MEM_AM_PERM_ACTION','Action');
+	define('_MEM_AM_PERM_ACCESS_HEADER','Access Header');
+	define('_MEM_AM_PERM_ACCESS_DESC','Access');
 
 	// Version 1.09
 	// About
-	define('_MEM_AM_ABOUT_MAKEDONATE', 'Improve membership make a donation!');
+	define('_MEM_AM_ABOUT_MAKEDONATE','Improve membership make a donation!');
 	
 	// Dashboard
-	define('_MEM_AM_ADMIN_COUNTS', 'Membership counts & sums');
-	define('_MEM_AM_ADMIN_THEREARE_INVOICES_UNPAID', 'There are %s membership invoices unpaid.');
-	define('_MEM_AM_ADMIN_THEREARE_INVOICES_PAID', 'There are %s membership invoices paid.');
-	define('_MEM_AM_ADMIN_THEREARE_PACKAGES', 'There are %s membership packages.');
-	define('_MEM_AM_ADMIN_AMOUNT_INVOICES', 'Total of Membership Invoices: %s');
-	define('_MEM_AM_ADMIN_NEXTRUN_PRELOAD', 'The preload cron will run again on: %s');
-	define('_MEM_AM_ADMIN_REQUIREMENTS', 'Membership Module Requirements');
-	define('_MEM_AM_ADMIN_PROFILE_FIELD', 'Profile Module Date/Time Field Name Required: %s');
-	define('_MEM_AM_EXISTS_PROFILE_FIELD', 'Does the required profile module field exist: %s');
+	define('_MEM_AM_ADMIN_COUNTS','Membership counts & sums');
+	define('_MEM_AM_ADMIN_THEREARE_INVOICES_UNPAID','There are %s membership invoices unpaid.');
+	define('_MEM_AM_ADMIN_THEREARE_INVOICES_PAID','There are %s membership invoices paid.');
+	define('_MEM_AM_ADMIN_THEREARE_PACKAGES','There are %s membership packages.');
+	define('_MEM_AM_ADMIN_AMOUNT_INVOICES','Total of Membership Invoices: %s');
+	define('_MEM_AM_ADMIN_NEXTRUN_PRELOAD','The preload cron will run again on: %s');
+	define('_MEM_AM_ADMIN_REQUIREMENTS','Membership Module Requirements');
+	define('_MEM_AM_ADMIN_PROFILE_FIELD','Profile Module Date/Time Field Name Required: %s');
+	define('_MEM_AM_EXISTS_PROFILE_FIELD','Does the required profile module field exist: %s');
 
 ?>
